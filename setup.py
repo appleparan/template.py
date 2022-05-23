f914b46caa748f563461be991c80fd10de986699
Jongsu "Liam" Kim <jongsukim8@gmail.com>
2022-05-23 15:21:05 +0900

    Update templates

---
+++ setup.py
@@ -0,0 +1,28 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="template",
+    packages=[],
+    version="0.1.0",
+    description="Description",
+    author="Jongsu Kim",
+    license="BSD",
+    author_email="jongsukim8@gmail.com",
+    url="https://github.com/USERNAME/REPO",
+    keywords=[
+        "template",
+        "package",
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: GPU :: NVIDIA CUDA :: 11.0",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
--- template/__init__.py
+++ template/__init__.py
@@ -1,1 +1,5 @@
-__version__ = '0.1.0'
+""""Top-level pacakge for the template module."""
+
+__author__ = """Jongsu Kim"""
+__email__ = "jongsukim8@gmail.com"
+__version__ = "0.1.0"
