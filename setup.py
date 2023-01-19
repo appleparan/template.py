191e06ebd64a8a27ccbeff726e04efedae8271a8
Jongsu "Liam" Kim <jongsukim8@gmail.com>
2023-01-20 07:36:58 +0900

    Use simpler setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-# !/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
-
-setup(
-    name="template",
-    packages=[],
-    version="0.1.0",
-    description="Description",
-    author="Jongsu Kim",
-    license="BSD",
-    author_email="jongsukim8@gmail.com",
-    url="https://github.com/USERNAME/REPO",
-    keywords=["template", "python", "project", "template-project"],
-    python_requires=">=3.9",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-)
+setup()
